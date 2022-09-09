15c454915a9c5f18fe82f06f4a828f6a8fdfa738
juandanieldiazocana <113211217+juandanieldiazocana@users.noreply.github.com>
2022-09-09 17:40:40 -0500

    Add files via upload

---
+++ Daniel Diaz actividad 2/arreglos.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+nombre=['juan','daniel','diaz','ocaña']
+nombre.reverse()
+nombre.pop('juan')
+nombre.clear()
+nombre.extend('inge')
+nombre.remove('juan')
+nombre.append('final')
+nombre.copy()
+print(nombre)
